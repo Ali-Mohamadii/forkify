6f650a66c4f8870c5d9c11e66e4a26bd1bdedffc
Ali <pachinco.a.m@gmail.com>
2023-07-06 16:20:07 +0330

    Initial commit

---
+++ src/js/Views/resultsView.js
@@ -0,0 +1,16 @@
+import Veiw from './View.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class ResultsView extends Veiw {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query. Please try againg ;)';
+  _message = '';
+
+  _generateMarkup() {
+    // console.log(this._data);
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultsView();
---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = ' 03f0cbde-3470-46d2-9acc-f7d10c5accf7';
+export const MODAL_CLOSE_SEC = 2.5;
